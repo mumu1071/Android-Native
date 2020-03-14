29afb4205393308b889315429ab0c0111f41b3ab
yangjie <yangjie1071@163.com>
2020-03-14 12:59:36 +0800

    添加JAVA测试

---
+++ app/src/main/java/com/yangjie/normal/demo/course/IPerson.java
@@ -0,0 +1,12 @@
+package com.yangjie.normal.demo.course;
+
+/**
+ * @author yangjie
+ * @description:
+ * @date :2020/3/14 11:39 AM
+ */
+public class IPerson {
+
+
+
+}
---
+++ javalib/src/main/java/com/yangjie/javalib/MyClass.java
@@ -0,0 +1,8 @@
+package com.yangjie.javalib;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
